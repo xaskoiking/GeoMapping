c2977afb8731a967ff2293cbc5a1746303eae377
xaskoiking <33464972+xaskoiking@users.noreply.github.com>
2018-10-18 19:35:31 -0400

    Updated Code

--- logic.js
+++ logic.js
@@ -1,36 +1,32 @@
+//Step 0: Declaring the variables ( API URL)
+var apiURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
 //Step 1: Creating map object
 var map = L.map("map", {
   center: [37.0902, -95.7129],
   zoom: 4
 });
 
-//Step 2: Create a function, which takes in the a value and multiply by 3 for the size of the circle
+
+//----------------- FUNCTIONS -----------------------------#
+//Step 2.1: Create a function, which takes in the a value and multiply by 3 for the size of the circle
 function markerSize(magnitude) {
     return magnitude * 3;
 };
 
-var markers = new L.FeatureGroup();
 
-// Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.light",
-  accessToken: API_KEY
-}).addTo(map);
-
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-
-function getColor(d) {
-    return d < 1 ?  'green' :
-           d < 2  ? 'yellowgreen' :
-           d < 3  ? 'forestgreen' :
-           d < 4  ? 'brown' :
+//Step 2.2: Create a function getColor, which will help in assiging the legened layer
+function getColor(col) {
+    return col < 1 ?  'green' :
+           col < 2  ? 'yellowgreen' :
+           col < 3  ? 'forestgreen' :
+           col < 4  ? 'brown' :
                      'red';
 }
 
-// Function that will determine the color of a neighborhood based on the borough it belongs to
+
+//Step 2.3: Create a function chooseColor, which will assign the color based on magnitude size.
 function chooseColor(mag) {
     if(mag < 1)
     {
@@ -54,21 +50,30 @@ function chooseColor(mag) {
     }
 }
 
+//Step 4: Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+}).addTo(map);
 
-d3.json(link, function(data) {
-  // Creating a geoJSON layer with the retrieved data
+
+//Step 5: Call the API and get the geojson data
+d3.json(apiURL, function(data) {
+  // 5.1) Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
-    // Style each feature (in this case a neighborhood)
+    //5.2)  Adding in the styles
     style: function(feature) {
       return {
         color: "white",
-        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        // 5.3) Select the color based on the magnitude
         fillColor: chooseColor(feature.properties.mag),
         fillOpacity: 0.5,
         weight: 1.5
       };
     },
-    // Called on each feature
+    // 5.4) On Each feature to add the pop up
     onEachFeature: function(feature, layer) {
       // Giving each feature a pop-up with information pertinent to it
       layer.bindPopup("<h1>" + feature.properties.place + "</h1> <hr> <h2>" + "Magnitude =" + feature.properties.mag + "</h2>");
@@ -84,6 +89,7 @@ d3.json(link, function(data) {
   }).addTo(map);
 });
 
+//Step 6: Adding the legend to the map
 var legend = L.control({position: 'bottomright'});
 
 legend.onAdd = function (map) {
